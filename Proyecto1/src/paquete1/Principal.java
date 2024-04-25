ed2941bbe34306b297f84100b7815e7895c204a7
juanm <juanm@DESKTOP-4PC91ML>
2024-04-25 17:37:08 +0200

    mensaje

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -3,5 +3,6 @@ package paquete1;
 public class Principal {
 public static void main (String [] arga) {
 	System.out.println("Estamos haciendo control de versiones con Git");
+	System.out.println("Estoy haciendo el ejercicio 3 de la tarea 4.5.2");
 }
 }
