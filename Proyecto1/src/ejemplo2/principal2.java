6b95b50c7c5a32b53158c0e510b8f2dc5c9881e6
juanm <juanm@DESKTOP-4PC91ML>
2024-05-09 17:50:24 +0200

    Cambio2

---
+++ Proyecto1/src/ejemplo2/principal2.java
@@ -0,0 +1,10 @@
+package ejemplo2;
+
+public class principal2 {
+	public static void main (String [] arga) {
+		System.out.println("Estamos haciendo control de versiones con Git");
+		System.out.println("Estoy haciendo el ejercicio 3 de la tarea 4.5.2");
+		System.out.println("Estoy haciendo el ejercicio 6 de la tarea 4.5.2");
+	}
+	
+}
